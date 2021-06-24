2e96a83415998c6dacaca680d5a556d36edd51d0
ismail harmanda <iharmanda@hotmail.com>
2021-06-24 20:37:07 +0300

    Some CSS added to scaffold

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from "react";
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
+  });
   return (
-    <div>
-      <h1>Dropwdown</h1>
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
     </div>
   );
 };
