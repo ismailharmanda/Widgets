8db1f4f54f3e0508c483df49dfd0edf97ebf86ec
ismail harmanda <iharmanda@hotmail.com>
2021-06-25 16:24:43 +0300

    Event Listener added to document.body in order to close dropdown menu.

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        setOpen(false);
+      },
+      { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) return null;
