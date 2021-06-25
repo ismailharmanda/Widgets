796969faaf54860f2df1c6d38a4fb75f62dbfe0d
ismail harmanda <iharmanda@hotmail.com>
2021-06-25 17:06:50 +0300

    Changed my mind after last commit. Used "useRef" in order to omptimize.

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
   useEffect(() => {
     document.body.addEventListener(
       "click",
       (event) => {
-        event.target.id === "body" && setOpen(false);
+        !ref.current.contains(event.target) && setOpen(false);
       },
       { capture: true }
     );
@@ -25,7 +26,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a color</label>
         <div
