f0d054ce32846b0702fa172fdf4917357b95795e
ismail harmanda <iharmanda@hotmail.com>
2021-06-26 02:43:04 +0300

    Translated Text has been displayed and timeout added for new api request

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -2,20 +2,32 @@ import React, { useState, useEffect } from "react";
 import googleTranslate from "../apis/googleTranslate";
 
 const Convert = ({ language, text }) => {
-    const [translated, setTranslated]
+  const [translated, setTranslated] = useState("");
   useEffect(() => {
-    googleTranslate.post(
-      "",
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-        },
-      }
+    const doTranslation = async () => {
+      const { data } = await googleTranslate.post(
+        "",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    const delayedTranslation = setTimeout(
+      text.length > 0 && doTranslation,
+      500
     );
+    return () => clearTimeout(delayedTranslation);
   }, [language, text]);
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
